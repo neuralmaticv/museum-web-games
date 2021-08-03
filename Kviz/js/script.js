93dfe9cd87c64671329076a4f8a5482937e08d4c
Vladimir Mijic <vlada.work.and.business@gmail.com>
2021-08-03 13:33:16 +0200

    formatiranje i male izmjene u kodu

--- Kviz/js/script.js
+++ Kviz/js/script.js
@@ -1,13 +1,20 @@
 // const data = require('./zagonetke.json');
 // console.log(data)
 
+import pitanjaLvl1 from "../data/pitanja1.js"
+import pitanjaLvl2 from "../data/pitanja2.js"
 import zagonetke from "../data/zagonetke.js"
 
+/*
+console.log(pitanjaLvl1);
+console.log(pitanjaLvl2);
 console.log(zagonetke);
+*/
+
 let start = document.querySelector("#start");
 
-let points = 0  // jedan tacan odgovor, jedan poen
-let i = 0
+let points = 0;  // jedan tacan odgovor, jedan poen
+let counter = 0;
 let back = document.querySelector("#back");
 let next = document.querySelector("#nextQuestion");
 let confirmAnswer = document.querySelector("#confirmAnswer")
@@ -19,84 +26,79 @@ let wrong = new Audio('Zvuci/wrong.wav');
 let correct = new Audio('Zvuci/correct.wav');
 let seconds = undefined;
 let randomNumbers = []
-let n = randomGenerator
-let countdown = undefined
+let n = randomGenerator;
+let countdown = undefined;
 
 
 function backToMenu() {
     document.getElementById('question0').style.display = "block";
     document.getElementById('question1').style.display = "none";
-    i = 0
-    points = 0
+    counter = 0;
+    points = 0;
 }
 
 function gameEnd() {
     question.textContent = "Imali ste " + points + " tačnih odgovora";
-    back.style.display = "inline"
-    confirmAnswer.style.display = "none"
-    next.style.display = "none"
+    back.style.display = "inline";
+    confirmAnswer.style.display = "none";
+    next.style.display = "none";
 }
 
 function newRiddle() {
-    if (i == 2) {
-        gameEnd()
+    if (counter == 10) {
+        gameEnd();
     }
     clearInterval(countdown);
-    document.getElementById("timer").textContent = 10
-    seconds = 10
+    document.getElementById("timer").textContent = 10;
+    seconds = 10;
     countdown = setInterval(function () {
         seconds--;
         document.getElementById("timer").textContent = seconds;
         if (seconds <= 0) {
             clearInterval(countdown);
-            question.textContent = "Vrijeme je isteklo"
+            question.textContent = "Vrijeme je isteklo";
             userAnswer.disabled = true;
             confirmAnswer.disabled = true;
             userAnswer.style.backgroundColor = 'red';
             wrong.play();
-            if (i == 2)
-                gameEnd()
+            if (counter == 10)
+                gameEnd();
 
         }
     }, 1000);
     userAnswer.disabled = false;
     confirmAnswer.disabled = false;
-    userAnswer.value = ""
+    userAnswer.value = "";
     userAnswer.style.backgroundColor = 'rgb(255, 255, 255)';
 
     let n;
     while (true) {
         n = randomNumber(9)
         if (!(n in randomNumbers)) {
-            randomNumbers.push(n)
-            i++
-            break
+            randomNumbers.push(n);
+            counter++;
+            break;
         }
     }
 
-    question.textContent = zagonetke[n].text;
-    trueAnswer = zagonetke[n].answer;
-    console.log(i)
-
+    question.textContent = zagonetke[n].question;
+    trueAnswer = zagonetke[n].correctAnswer;
+    console.log("Pitanje broj %d, odgovor = %s;", counter, trueAnswer);
 }
 
 function isAnswerTrue() {
-
-
-
-
     if (seconds > 0) {
         clearInterval(countdown);
         if (userAnswer.value.toUpperCase() === trueAnswer.toUpperCase()) {
-            console.log("tacno")
+            console.log("tacno");
             points++
-            userAnswer.style.backgroundColor = 'rgb(85, 166, 48)';
+            userAnswer.style.backgroundColor = 'rgb(125, 235, 52)';
             correct.play();
 
         }
 
         else {
-            console.log("netacno")
+            console.log("netacno");
             userAnswer.style.backgroundColor = 'rgb(230, 57, 70)';
             wrong.play();
         }
@@ -104,20 +106,17 @@ function isAnswerTrue() {
         userAnswer.disabled = true;
         confirmAnswer.disabled = true;
 
-        if (i == 2)
-            gameEnd()
-
-
+        if (counter == 10)
+            gameEnd();
     }
-
 }
 
 function startQuiz() {
     document.getElementById('question0').style.display = "none";
     document.getElementById('question1').style.display = "block";
-    back.style.display = "none"
-    confirmAnswer.style.display = "inline"
-    next.style.display = "inline"
+    back.style.display = "none";
+    confirmAnswer.style.display = "inline";
+    next.style.display = "inline";
     newRiddle();
 }
 
@@ -128,17 +127,17 @@ function randomNumber(max) {
 }
 
 function randomGenerator() {
-    let randomNumbers = []
+    let randomNumbers = [];
     for (let i = 0; i < 4; i++) {
-        let n = randomNumber(9)
+        let n = randomNumber(9);
         if (!(n in randomNumbers)) {
-            randomNumbers.push(n)
+            randomNumbers.push(n);
         }
     }
-    return randomNumbers
+    return randomNumbers;
 }
 
-start.addEventListener("click", startQuiz)
-next.addEventListener("click", newRiddle)
-confirmAnswer.addEventListener("click", isAnswerTrue)
-back.addEventListener("click", backToMenu)
+start.addEventListener("click", startQuiz);
+next.addEventListener("click", newRiddle);
+confirmAnswer.addEventListener("click", isAnswerTrue);
+back.addEventListener("click", backToMenu);
