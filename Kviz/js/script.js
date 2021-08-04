25fb622d9f6acc4ed4fb1934d54f2d5c328b02a5
Vladimir Mijic <vlada.work.and.business@gmail.com>
2021-08-04 02:41:30 +0200

    uveden drugi format pitanja u kviz

--- Kviz/js/script.js
+++ Kviz/js/script.js
@@ -1,22 +1,16 @@
-// const data = require('./zagonetke.json');
-// console.log(data)
-
 import pitanjaLvl1 from "../data/pitanja1.js"
-import pitanjaLvl2 from "../data/pitanja2.js"
 import zagonetke from "../data/zagonetke.js"
 
-/*
-console.log(pitanjaLvl1);
-console.log(pitanjaLvl2);
-console.log(zagonetke);
-*/
 
-let start = document.querySelector("#start");
+const startButton = document.getElementById("start");
+const backButton = document.getElementById("back");
+const nextButton1 = document.getElementById("nextQuestionType1");
+const nextButton2 = document.getElementById("nextQuestionType2");
+
 
 let points = 0;  // jedan tacan odgovor, jedan poen
 let counter = 0;
-let back = document.querySelector("#back");
-let next = document.querySelector("#nextQuestion");
+
 let confirmAnswer = document.querySelector("#confirmAnswer")
 let userAnswer = document.querySelector("#userAnswer");
 let trueAnswer = ""
@@ -30,23 +24,36 @@ let n = randomGenerator;
 let countdown = undefined;
 
 
+
+const homeScreen = document.getElementById("homeScreen");
+const questionFormT1 = document.getElementById("questionType1");
+const questionFormT2 = document.getElementById("questionType2");
+
+
+const questionElement = document.getElementById("pitanjeT1");
+const answerButtons = document.getElementById("odgovoriT1");
+
+let currentQuestionIndex, shuffledQuestions;
+
+
 function backToMenu() {
-    document.getElementById('homeScreen').style.display = "block";
-    document.getElementById('questionType1').style.display = "none";
-    document.getElementById('questionType2').style.display = "none";
-    counter = 0;
-    points = 0;
+    homeScreen.classList.remove("hide");
+    questionFormT1.classList.add("hide");
+    questionFormT2.classList.add("hide");
+
+    points = counter = 0;
 }
 
 function gameEnd() {
     question.textContent = "Imali ste " + points + " tačnih odgovora";
-    back.style.display = "inline";
-    confirmAnswer.style.display = "none";
-    next.style.display = "none";
+
+    backButton.classList.remove("hide");
+    confirmAnswer.classList.add("hide");
+    nextButton2.classList.add("hide");
 }
 
 function newRiddle() {
-    if (counter == 5) {
+    if (counter == 10) {
         gameEnd();
     }
     clearInterval(countdown);
@@ -62,7 +69,7 @@ function newRiddle() {
             confirmAnswer.disabled = true;
             userAnswer.style.backgroundColor = 'red';
             wrong.play();
-            if (counter == 5)
+            if (counter == 10)
                 gameEnd();
 
         }
@@ -84,14 +91,13 @@ function newRiddle() {
 
     question.textContent = zagonetke[n].question;
     trueAnswer = zagonetke[n].correctAnswer;
-    console.log("Pitanje broj %d, odgovor = %s;", counter, trueAnswer);
+    console.log("Pitanje broj %d, odgovor = %s; POINTS %d", counter, trueAnswer, points);
 }
 
 function isAnswerTrue() {
     if (seconds > 0) {
         clearInterval(countdown);
         if (userAnswer.value.toUpperCase() === trueAnswer.toUpperCase()) {
-            console.log("tacno");
             points++
             userAnswer.style.backgroundColor = 'rgb(125, 235, 52)';
             correct.play();
@@ -99,7 +105,6 @@ function isAnswerTrue() {
         }
 
         else {
-            console.log("netacno");
             userAnswer.style.backgroundColor = 'rgb(230, 57, 70)';
             wrong.play();
         }
@@ -107,22 +112,24 @@ function isAnswerTrue() {
         userAnswer.disabled = true;
         confirmAnswer.disabled = true;
 
-        if (counter == 5)
+        if (counter == 10)
             gameEnd();
     }
 }
 
 function startQuiz() {
-    document.getElementById('homeScreen').style.display = "none";
-    document.getElementById('questionType1').style.display = "none";
-    document.getElementById('questionType2').style.display = "block";
-    back.style.display = "none";
-    confirmAnswer.style.display = "inline";
-    next.style.display = "inline";
-    newRiddle();
+    homeScreen.classList.add("hide");
+    questionFormT1.classList.remove("hide");
+    questionFormT2.classList.add("hide");
+
+    backButton.classList.add("hide");
+    confirmAnswer.classList.add("hide");
+    nextButton2.classList.add("hide");
+
+    shuffledQuestions = pitanjaLvl1.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    setQuestion();
 }
-
-
 
 function randomNumber(max) {
     return Math.floor(Math.random() * max);
@@ -139,7 +146,85 @@ function randomGenerator() {
     return randomNumbers;
 }
 
-start.addEventListener("click", startQuiz);
-next.addEventListener("click", newRiddle);
+startButton.addEventListener("click", startQuiz);
 confirmAnswer.addEventListener("click", isAnswerTrue);
-back.addEventListener("click", backToMenu);
+backButton.addEventListener("click", backToMenu);
+nextButton1.addEventListener("click", () => {
+    currentQuestionIndex++;
+    setQuestion();
+});
+nextButton2.addEventListener("click", newRiddle);
+
+
+
+function setQuestion() {
+    resetPrevState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(q) {
+    questionElement.innerText = q.question;
+
+    let chr;
+    for (chr in q.answers) {
+        const button = document.createElement("button");
+        button.innerHTML = "<b>" + chr.toUpperCase() + ")</b>" + q.answers[chr];
+        button.classList.add("btn-odgovor");
+
+        button.addEventListener("click", function(event) {
+            const selectedAnswer = event.target.innerHTML[3];
+            const correctAnswer = q.correctAnswer;
+
+            if (selectedAnswer === correctAnswer.toUpperCase()) {
+                points++;
+                correct.play();
+            } else {
+                wrong.play();
+            }
+
+            setStatusClass(document.body, selectedAnswer, correctAnswer);
+            Array.from(answerButtons.children).forEach(button => {
+                setStatusClass(button, button.innerHTML[3], correctAnswer);
+            });
+
+            if (counter == 5) {
+                questionFormT1.classList.add("hide");
+                questionFormT2.classList.remove("hide");
+            
+                confirmAnswer.classList.remove("hide");
+                nextButton2.classList.remove("hide");
+        
+                newRiddle();
+            } else {
+                nextButton1.classList.remove("hide");
+            }
+        });
+        answerButtons.appendChild(button);
+    }
+
+    counter++;
+    console.log("Pitanje broj %d, odgovor = %s; POINTS %d", counter, q.correctAnswer, points);
+}
+
+function resetPrevState() {
+    nextButton1.classList.add("hide");
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+
+function setStatusClass(element, selected, correct) {
+    clearStatusClass(element);
+
+    if (selected === correct.toUpperCase()) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
+}
