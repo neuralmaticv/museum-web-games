c9a492bb1918d7b3da0fe70521223b05cbd0bddd
Vladimir Mijic <vlada.work.and.business@gmail.com>
2021-08-04 22:18:10 +0200

    formatiranje pitanja

--- Kviz/data/pitanja1.js
+++ Kviz/data/pitanja1.js
@@ -73,6 +73,52 @@ const pitanjaLvl1 = [
             b: "dvopolni i jednopolni"
         },
         correctAnswer: "a"
+    },
+    {
+        question: "Korijen koje biljke se upotrebljava kod zarastanja lomova kostiju i tkiva?",
+        answers: {
+            a: "plućnjaka",
+            b: "sljeza",
+            c: "gaveza"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Zbog svoje izuzetne ljekovitosti, za koju biljku se u narodu kaže: neće grom u _______?",
+        answers: {
+            a: "kamilicu",
+            b: "koprive",
+            c: "pelin"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Koja od navedenih biljaka se ne koristi za liječenje bolesti disajnih organa?",
+        answers: {
+            a: "jagorčevina",
+            b: "bokvica",
+            c: "neven",
+            d: "bršljen"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "List koje biljke se koristi za zarastanje rana na tijelu?",
+        answers: {
+            a: "žalfije",
+            b: "matičnjaka",
+            c: "hajdučke trave"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Za koju biljku se u narodu kaže da njezin cvijet usred ljeta skuplja sunčeve zrake, da bih ih dao ljudima za vrijeme tmurnih mjeseci?",
+        answers: {
+            a: "kantarion",
+            b: "zlatnica",
+            c: "žutilovka"
+        },
+        correctAnswer: "a"
     }
 ];
 
