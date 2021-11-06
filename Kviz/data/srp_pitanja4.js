aab18cd3001bb1a6d883620f45ae63a42b93f3b0
Vladimir Mijic <vlada.work.and.business@gmail.com>
2021-11-06 13:03:41 +0100

    changed questions from latin to cyrillic

---
+++ Kviz/data/srp_pitanja4.js
@@ -0,0 +1,36 @@
+const pitanjaLvl4 = [
+  {
+    picture: "images/kamilica.jpg",
+    answers: [
+      "камилица",
+      "маслачак"
+    ],
+    correctAnswer: "камилица"
+  },
+  {
+    picture: "images/bokvica-zenska.jpg",
+    answers: [
+      "мушка боквица",
+      "женска боквица"
+    ],
+    correctAnswer: "женска боквица"
+  },
+  {
+    picture: "images/crvena-djetalina.jpg",
+    answers: [
+      "црвена дјетелина",
+      "бијела дјетелина"
+    ],
+    correctAnswer: "црвена дјетелина"
+  },
+  {
+    picture: "images/crni-sljez.jpg",
+    answers: [
+      "бијели сљез",
+      "црни сљез"
+    ],
+    correctAnswer: "црни сљез"
+  }
+];
+
+export default pitanjaLvl4
