013d70c81e2b3889d158ebd32ee7abef54a6db40
Vladimir Mijic <vlada.work.and.business@gmail.com>
2021-11-17 17:35:21 +0100

    update - multi-lang implementation #4

---
+++ Kviz/data/eng_pitanja2.js
@@ -0,0 +1,56 @@
+const engPitanjaLvl2 = [
+    {
+        "id": 1,
+        "question": "I am yellow, but I am not bees, if you need vit C then you need to squeeze.",
+        "correctAnswer": "Lemon"
+    },
+    {
+        "id": 2,
+        "question": "My body is woody, in a green suit, in the shadow glistens a juicy purple fruit.",
+        "correctAnswer": "Grapes"
+    },
+    {
+        "id": 3,
+        "question": "A sturdy house, no door, in it live a family of four.",
+        "correctAnswer": "Walnut"
+
+    },
+    {
+        "id": 4,
+        "question": "A green flame fluttering in the wind burning, hot giving you a sting.",
+        "correctAnswer": "Nettle"
+
+    },
+    {
+        "id": 5,
+        "question": "I’m a white bark tree, with lines as grey as a church. With a graceful pose, now it’s clear I’m a ______.",
+        "correctAnswer": "Birch"
+
+    },
+    {
+        "id": 6,
+        "question": "I am very spicy, when you put me in a pie. But when you cut me, I will make you cry.",
+        "correctAnswer": "Onion"
+
+    },
+    {
+        "id": 7,
+        "question": "First, their heads are yellow laying all around the meadow. And then in the end they wear parachute instead of hair.",
+        "correctAnswer": "Dandelion"
+
+    },
+    {
+        "id": 8,
+        "question": "In the spring, I make you feel tranquil. In the summer, I help you chill. In the autumn, I give you food. In the winter, I give you firewood. ",
+        "correctAnswer": "Tree"
+
+    },
+    {
+        "id": 9,
+        "question": "I’m a strong tree, easy in the woods to recognize me. All the squirrels want to find, my children scattered around.",
+        "correctAnswer": "Oak"
+
+    }
+];
+
+export default engPitanjaLvl2
---
+++ Kviz/data/eng_pitanja3.js
@@ -0,0 +1,74 @@
+const engPitanjaLvl3 = [
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "A M L O C H E M I",
+        correctAnswer: "chamomile",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "D L R N A E V E",
+        correctAnswer: "lavender",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "T I M N",
+        correctAnswer: "mint",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "A P S E R Y B R R",
+        correctAnswer: "raspberry",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "R I C H B",
+        correctAnswer: "birch",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "K O A",
+        correctAnswer: "oak",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "C E B E H",
+        correctAnswer: "beech",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "G O K I N G",
+        correctAnswer: "ginkgo",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "E L E T T N",
+        correctAnswer: "nettle",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "N I P E",
+        correctAnswer: "pine",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "G I F",
+        correctAnswer: "fig",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "V I Y",
+        correctAnswer: "ivy",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "S L E N D I N",
+        correctAnswer: "lindens",
+    },
+    {
+        question: "Put the letters in the correct place to get the correct name of the plant",
+        letters: "M E N L O",
+        correctAnswer: "lemon",
+    }
+];
+
+export default engPitanjaLvl3
