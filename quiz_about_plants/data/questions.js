02461865e500580e1fdcfa48f253bd0e7f7a9379
unknown <kjelenajeca55@gmail.com>
2022-01-16 17:05:55 +0100

    Update

--- quiz_about_plants/data/questions.js
+++ quiz_about_plants/data/questions.js
@@ -44,29 +44,64 @@ const questions = [
     },
     {
         id: 6,
-        picture: "images/neven.png",
-        correctAnswer: "Невен",
-        explanation: "Невен се узгаја као украсна и љековита биљка. Најчешће се употребљава у виду" +
-            "„невенове масти“ за зарастање рана и других кожних обољења, а у виду чаја користи се за" +
-            "лијечење желуца и цријева."
+        picture: "images/vodopija.png",
+        correctAnswer: "Водопија"
+    
     },
     {
         id: 7,
-        picture: "images/maslacak (2).png",
-        correctAnswer: "Маслачак",
-        explanation: "Маслачак је зељаста биљка са меснатим коријеном и издуженим неправилно урезаним" +
-            "листовима, скупљени у приземну розету. Стабљика је шупља пуна млијечног сока, а на" +
-            "врху се налази цвјетна главица жуте боје. Сви дијелови биљке су љековити, а листови се" +
-            "користе и у исхрани у виду салате."
+        picture: "images/istockphoto-612402456-170667a-removebg-preview.png",
+        correctAnswer: "Бршљан"
+  
 
     },
     {
         id: 8,
-        picture: "images/hmelj.png",
-        correctAnswer: "Хмељ",
-        explanation: "Хмељ садржи супстанце које имају умирујући и успављујући ефекат. Народно искуство" +
-            "и научна испитивања показала су да је хмељ одлично средство против несанице, па се у" +
-            "новије вријеме производе јастуци и душеци пуњени цвастима хмеља за бољи сан."
+        picture: "images/crocus-one-first-spring-flowers-260nw-762940639-removebg-preview.png",
+        correctAnswer: "Љубичица"
+        
+    },
+    {
+        id: 9,
+        picture: "images/istockphoto-1180142132-612x612-removebg-preview.png",
+        correctAnswer: "Дјеталина"
+       
+    },
+    {
+        id: 10,
+        picture: "images/lipa.png",
+        correctAnswer: "Липа"
+       
+    },
+    {
+        id: 11,
+        picture: "images/nnana-removebg-preview.png",
+        correctAnswer: "Нана"
+       
+    },
+    {
+        id: 12,
+        picture: "images/istockphoto-1180142132-612x612-removebg-preview.png",
+        correctAnswer: "Дјеталина"
+       
+    },
+    {
+        id: 13,
+        picture: "images/istockphoto-1180142132-612x612-removebg-preview.png",
+        correctAnswer: "Дјеталина"
+       
+    },
+    {
+        id: 14,
+        picture: "images/istockphoto-1180142132-612x612-removebg-preview.png",
+        correctAnswer: "Дјеталина"
+       
+    },
+    {
+        id: 15,
+        picture: "images/istockphoto-1180142132-612x612-removebg-preview.png",
+        correctAnswer: "Дјеталина"
+       
     }
 ];
 
